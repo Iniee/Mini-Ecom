7eb65cfd4040b398f01864df675dfea334abc110
Iniee <66095409+Iniee@users.noreply.github.com>
2021-10-23 11:35:33 +0100

    changes

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$currency = '#';
+$db_username = 'qus9qnmk6o2tgy9a';
+$db_password = 'fppt4htg6g1wj6re ';
+$db_name = 'bolt';
+$db_host = 'en1ehf30yom7txe7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
+?>
